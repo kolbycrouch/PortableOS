3485556f739332055462e5d297de3b757a9a9b97
root <root@i64-build.attlocal.net>
2019-02-09 03:47:38 -0600

    Lots of clean-up. Initial kernel support

--- appimage/setup.sh
+++ appimage/setup.sh
@@ -1,8 +1,8 @@
-ln -sfn /tmp/.mount_sdk-linux-musl-a64* /tmp/.pkg-sdk-linux-musl-a64HLKeDE
+ln -sfn /tmp/.mount_REPLACEPKGNAME* REPLACEPREFIX
 cd $OWD
 unset LD_LIBRARY_PATH
-export PATH=/tmp/.pkg-sdk-linux-musl-a64HLKeDE/bin:/tmp/.pkg-sdk-linux-musl-a64HLKeDE/sbin:$PATH
-export CC=/tmp/.pkg-sdk-linux-musl-a64HLKeDE/bin/gcc
-export CXX=/tmp/.pkg-sdk-linux-musl-a64HLKeDE/bin/g++
-export PKG_CONFIG_LIBDIR=/tmp/.pkg-sdk-linux-musl-a64HLKeDE/lib/pkgconfig
-bash
+export PATH=REPLACEPREFIX/bin:REPLACEPREFIX/sbin:$PATH
+export CC=REPLACEPREFIX/bin/gcc
+export CXX=REPLACEPREFIX/bin/g++
+export PKG_CONFIG_LIBDIR=REPLACE/lib/pkgconfig
+$SHELL
